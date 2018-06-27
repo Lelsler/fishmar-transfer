680f5c824f325a739e9f1f362331942c6a4d3885
Maartje Oosdijk <mtn1@uppsetnings-MacBook-Pro-5.local>
2018-06-27 11:36:36 +0100

    changes to dataframe file
    Former-commit-id: f59b07a7c96b2290eae44f260eee638a48f57c61

--- RQ2/code/data_frame_composing.R
+++ RQ2/code/data_frame_composing.R
@@ -12,7 +12,7 @@ setwd("/Users/mtn1/Desktop/R_projects/fishmar/data")
 #load governance effectiveness data from worldbank. The indicator is an estimate, I'm not considering the standard deviations and confidence intervals that are in the original data
 gov_eff = read.csv("/Users/mtn1/Desktop/R_projects/fishmar/data/World_bank_data/governance_effective.csv") 
 #load stock assessment data based on the landings reported to FAO
-data = read.csv("/Users/mtn1/Desktop/R_projects/fishmar/data/fao_stock_status/processed/1950_2017_FAO_bbmsy_timeseries_merge.csv")
+stock = read.csv("/Users/mtn1/Desktop/R_projects/fishmar/data/fao_stock_status/processed/1950_2017_FAO_bbmsy_timeseries_merge.csv")
 #load Comtrade data
 trade = read.csv("/Users/mtn1/Desktop/R_projects/fishmar/data/com_trade/CT_max_toothfish_29Jan18.csv") 
 
@@ -47,7 +47,7 @@ CT_trade = merge(trade, df_CT, all=TRUE)
 CT_trade = merge(CT_trade, df_CT2, all=TRUE)
 
 #filter the stock estimates for patagonian toothfish
-data = data %>% filter(comm_name == "Patagonian toothfish")
+data = stock %>% filter(comm_name == "Patagonian toothfish")
 data$Year = data$year
 
 #take out a very general commodity class of the patagonian toothfish, but check later if it's needed
@@ -74,9 +74,36 @@ sd(na.omit(tryout$geff.Exporter))
 l = lm(tryout$geff.Importer ~ tryout$Year)
 summary(l)
 
-boxplot(tryout$geff.Importer ~ tryout$Year)
+#plots with exporter governance changes over time
+boxplot(tryout$geff.Exporter ~ tryout$Year)
 
-boxplot(tryout$dist ~ tryout$Year)
+#boxplot(tryout$dist ~ tryout$Year)
 
+# construct network from trade data
+
+#construct network
+
+edgelist = tryout[, 1:2]
+
+tradenetwork = igraph::simplify(igraph::graph.data.frame(edgelist, directed=TRUE))
+nodeList = data.frame(Name = igraph::V(tradenetwork)$name, 
+                       ID = c(0:(igraph::vcount(tradenetwork) - 1))) # create nodelist from tradenetwork
+nodeList <- cbind(nodeList, nodeDegree=igraph::degree(tradenetwork, v = igraph::V(tradenetwork), mode = "all")) #calculate node degree for all nodes in network
+
+
+plot(tradenetwork, vertex.color="pink", edge.color ="dark grey",
+     vertex.size=nodeList$nodeDegree,
+     edge.arrow.size=.1, vertex.label.color="black", vertex.label.cex=.2, rescale=FALSE)
+
+plot(tradenetwork,  vertex.label.color="black", vertex.color="pink", vertex.size=nodeList$nodeDegree, edge.arrow.size=.1, ertex.label.color="black", vertex.label.cex=.7)
+
+tryout1 = tryout[tryout$Exporter.ISO=="ARG", ]
+plot(tryout1$Year, tryout1$super, xlab="Year", ylab= "Stock status ARG", pch=20, ylim=c(0,1.2), col="blue", main="Stock status ARG")
+
+tryout2 = tryout[tryout$Exporter.ISO=="CHL", ]
+plot(tryout2$Year, tryout2$super, xlab="Year", ylab= "Stock status CHL", pch=20, ylim=c(0,1.2), col="blue", main="Stock status CHL")
+
+tryout3 = tryout[tryout$Exporter.ISO=="FLK", ]
+plot(tryout3$Year, tryout3$super, xlab="Year", ylab= "Stock status FLK", pch=20, ylim=c(0,1.2), col="blue", main="Stock status CHL")
 
 
